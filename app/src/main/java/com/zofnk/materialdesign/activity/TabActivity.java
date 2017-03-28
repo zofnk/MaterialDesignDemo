8e8008186b292dc60ceea3700b2e1fc5463f589f
zofnk <zofnk@vip.qq.com>
2017-03-28 17:16:56 +0800

    Detial 可换title

--- app/src/main/java/com/zofnk/materialdesign/activity/TabActivity.java
+++ app/src/main/java/com/zofnk/materialdesign/activity/TabActivity.java
@@ -3,11 +3,17 @@ package com.zofnk.materialdesign.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.zofnk.materialdesign.R;
 import com.zofnk.materialdesign.adapter.MainPageAdapter;
@@ -17,7 +23,10 @@ import com.zofnk.materialdesign.fragment.MineFragment;
 
 public class TabActivity extends AppCompatActivity {
 
-    private Toolbar mToolBar;
+    private CollapsingToolbarLayout mCollapsingToolBarLayout;
+    private ImageView mIcon;
+    private ViewPager mVp;
+    private TabLayout mTabLayout;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, TabActivity.class);
@@ -40,11 +49,55 @@ public class TabActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
-        ViewPager vp = (ViewPager) findViewById(R.id.viewPager);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        mToolBar = (Toolbar) findViewById(R.id.toolbar);
-        mToolBar.setTitle("Detial Info");
-        vp.setAdapter(new MainPageAdapter(getSupportFragmentManager(), mFragments, tabs));
-        tabLayout.setupWithViewPager(vp);
+        initViews();
+        init();
+    }
+
+    private void initViews() {
+        mVp = (ViewPager) findViewById(R.id.viewPager);
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mIcon = (ImageView) findViewById(R.id.imgIcon);
+    }
+
+    private void init() {
+        mCollapsingToolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolBarLayout);
+        mVp.setAdapter(new MainPageAdapter(getSupportFragmentManager(), mFragments, tabs));
+        mTabLayout.setOnTabSelectedListener(new ViewPagerOnTabSelectedListener());
+        mTabLayout.setupWithViewPager(mVp);
+    }
+
+    public void setToolBarBackground(@DrawableRes int resId) {
+        mIcon.setImageResource(resId);
+    }
+
+    public void setToolbarTitle(String title) {
+        mCollapsingToolBarLayout.setTitle(title);
+    }
+
+    private class ViewPagerOnTabSelectedListener extends TabLayout.ViewPagerOnTabSelectedListener {
+        public ViewPagerOnTabSelectedListener() {
+            super(TabActivity.this.mVp);
+        }
+
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            Log.e("onTabSelected : ", "" + tab.getPosition());
+            switch (tab.getPosition()) {
+                case 0:
+                    setToolbarTitle("Home Detial");
+//                        setToolBarBackground(R.mipmap.image);
+                    break;
+                case 1:
+                    setToolbarTitle("Find Detial");
+//                        setToolBarBackground(R.mipmap.image1);
+                    break;
+                case 2:
+                    setToolbarTitle("Mine Detial");
+//                        setToolBarBackground(R.mipmap.image);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/zofnk/materialdesign/fragment/FindFragment.java
+++ app/src/main/java/com/zofnk/materialdesign/fragment/FindFragment.java
@@ -3,11 +3,14 @@ package com.zofnk.materialdesign.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zofnk.materialdesign.R;
+import com.zofnk.materialdesign.adapter.MainAdapter;
 
 
 /**
@@ -26,15 +29,19 @@ public class FindFragment extends Fragment {
     }
 
     public FindFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_find, container, false);
+        RecyclerView mRecyclerView = (RecyclerView) view.findViewById(R.id.rv);
+        LinearLayoutManager lm = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(lm);
+        MainAdapter mainAdapter = new MainAdapter(getContext());
+        mRecyclerView.setAdapter(mainAdapter);
+        mainAdapter.setOnItemClickListence(null);
+        return view;
     }
 
 }
